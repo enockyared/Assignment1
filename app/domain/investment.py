9af839d521116e939dcc3ac4689f2c0edbfa1f80
Your Name <your@email.com>
2025-11-04 20:45:23 -0600

    Remove all files from main

--- app/domain/investment.py
+++
@@ -1,8 +0,0 @@
-class Investment:
-    def __init__(self, ticker: str, quantity: int, purchase_price: float):
-        self.ticker = ticker
-        self.quantity = quantity
-        self.purchase_price = purchase_price
-
-    def __repr__(self):
-        return f"{self.ticker}: {self.quantity} @ ${self.purchase_price}"
--- main.py
+++
@@ -1,8 +0,0 @@
-from app.cli.login_menu import login_menu
-
-def main():
-    login_menu()
-
-if __name__ == "__main__":
-    main()
-
